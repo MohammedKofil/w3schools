539ad2456914876d9fff81b1f8aa6d0256896e19
Mohammed Kofil <mohammedkofil8@gmail.com>
2023-02-17 21:16:56 +0600

    Done

--- script.js
+++ script.js
@@ -19,18 +19,91 @@
 // let total = price1 + price2;
 // console.log("The total is ; "+ total);
 
-let laptoName = "Hp", laptopModel = "Pavilion", laptoColor = "Black";
-let laptop;
-let $ = "Hello World";
-let $$$ = 2;
-let $myMoney = 500;
-let x = 2 + 3 + 4;
-const pi = 3.1416;
-let person = "Mohammed Kofil";
-let answer = "Yes! I am...";
-let carName = "Volvo"
-let _lastName = "Kofili", _h = 2, _100 = 23;
-console.log("Hi " + _lastName + " this is " + person + " using " + laptoName + " " + answer + " Driving " + carName + x + $ + $$$ + $myMoney);
+// let laptoName = "Hp", laptopModel = "Pavilion", laptoColor = "Black";
+// let laptop;
+// let $ = "Hello World";
+// let $$$ = 2;
+// let $myMoney = 500;
+// let x = 2 + 3 + 4;
+// const pi = 3.1416;
+// let person = "Mohammed Kofil";
+// let answer = "Yes! I am...";
+// let carName = "Volvo"
+// let _lastName = "Kofili", _h = 2, _100 = 23;
+// console.log("Hi " + _lastName + " this is " + person + " using " + laptoName + " " + answer + " Driving " + carName + x + $ + $$$ + $myMoney);
 
+
+// const a = 2, b = 32, c = 53;
+// let x = (a + b - c) * a / c;
+// let y = b ** a;
+// let r = c % a;
+// y++;
+// a--;
+
+
+
+// let t = 2, v = 4;
+// t = v;
+// t += v;
+// t -= v;
+// t *= v;
+// t /= v;
+// t %= v;
+// t **= v;
+
+// x <<= y;
+// x >>= y;
+// x >>>= y;
+
+// x &= y;
+// x ^= y;
+// x |= y;
+
+// x &&= y;
+// x ||= y;
+// x ??= y;
+
+
+// let text1 = "Moh", text2 = "ammed";
+// let text3 = text1 + text2;
+// let text4 = "What a very ";
+// text4 += "nice day Broh!"
+// console.log(text4);
+
+
+// console.log(x, y, r)
+
+//operator odre
+// PEMDAS BODMAS
+
+
+//Data types
+
+let length = 16;
+let weight = 7.4;
+let num1 = 123e5;
+let num2 = 123e-5;
+
+let b = BigInt("6325425463264124455565435746354357654");
+
+
+
+let color = "Yellow";
+let lastName = "Kofil";
+
+let x = true;
+let y = false;
+
+const person = {firstName: "Mohammed", lastName: "Kofil"};
+
+const cars = ["Volvo", "Lamborgini", "Ford"];
+
+const date = new Date("2022-02-17");
+
+// let x = 5;
+// let y = 5;
+// let z = 6;
+
+console.log(typeof(cars), typeof(date), typeof(person), typeof(color, lastName), typeof(x));
 
 
