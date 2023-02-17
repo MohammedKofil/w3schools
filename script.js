8bb1d533b31de18fb564221d03fa07dbbe0ed357
Mohammed Kofil <mohammedkofil8@gmail.com>
2023-02-17 20:18:01 +0600

    Starting

---
+++ script.js
@@ -0,0 +1,36 @@
+// console.log(5+45);
+
+// let x, y, z;
+// x = 3;
+// y = 2;
+// z = 22;
+// z = x + y;
+
+// let x;
+// let y;
+
+// x = 5;
+// y = 6;
+// let z = (x + y) * x / y - x;
+// console.log(z);
+
+// const price1 = 1;
+// const price2 = 23;
+// let total = price1 + price2;
+// console.log("The total is ; "+ total);
+
+let laptoName = "Hp", laptopModel = "Pavilion", laptoColor = "Black";
+let laptop;
+let $ = "Hello World";
+let $$$ = 2;
+let $myMoney = 500;
+let x = 2 + 3 + 4;
+const pi = 3.1416;
+let person = "Mohammed Kofil";
+let answer = "Yes! I am...";
+let carName = "Volvo"
+let _lastName = "Kofili", _h = 2, _100 = 23;
+console.log("Hi " + _lastName + " this is " + person + " using " + laptoName + " " + answer + " Driving " + carName + x + $ + $$$ + $myMoney);
+
+
+
